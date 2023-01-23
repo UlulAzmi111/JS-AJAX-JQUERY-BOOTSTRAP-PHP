9a732f54dd94e03ab7e042ca961e0d20d82e06af
UlulAzmi111 <azmiulul1453@gmail.com>
2023-01-23 12:35:27 +0700

    Memodifikasi file index.php untuk video ke 34 -> Pembuatan Layout

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <title>Ajax-Jquery Bootstrap PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
             rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -11,8 +11,71 @@
   </head>
 
   <body>
-    <h1>Belajar Ajax-Jquery Bootstrap PHP</h1>
+    
+    <div class="container text-center">
+        <div class="row mt-4" style="text-align: left;">
+            <h1>Belajar Ajax-Jquery Bootstrap PHP</h1>
+        </div>
+
+        <div class="row mt-4" style="text-align: left;">
+            <div class="col">
+                <div class="row">
+                    <h2>Input Data Pelanggan</h2>
+                </div>
+                <div class="row">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Pelanggan</label>
+                            <input type="text" class="form-control" id="pelanggan" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">Harus diisi</div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Alamat</label>
+                            <input type="text" class="form-control" id="alamat">
+                            <div id="emailHelp" class="form-text">Harus diisi</div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Telp</label>
+                            <input type="text" class="form-control" id="telp">
+                            <div id="emailHelp" class="form-text">Harus diisi</div>
+                        </div>
+
+                        <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <h2>Data Pelanggan</h2>
+                </div>
+                <div class="row">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Pelanggan</th>
+                                <th scope="col">Alamat</th>
+                                <th scope="col">Telp</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
             integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
   </body>
 </html>
